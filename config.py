7635a01f7288472864e7d079b500c533deff429a
Sourav B. Roy <wildr.slimshady@gmail.com>
2024-04-30 16:28:17 +0530

    - add new config file store urls - click on Go! to reach main dresses page

---
+++ config.py
@@ -0,0 +1,1 @@
+URL_FOR_DRESSES = 'https://www.bershka.com/ww/women/clothes/dresses-c1010193213.html'
--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-url = 'https://www.bershka.com/ww/women/clothes/dresses-c1010193213.html'
-url1 = 'https://www.instagram.com'
 import asyncio
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
+from config import URL_FOR_DRESSES
+import re
 
 
 async def main():
-
     import asyncio
+
+
 from playwright.async_api import async_playwright
+
 
 async def main():
     async with async_playwright() as p:
         browser = await p.firefox.launch(headless=False)
         page = await browser.new_page()
-        await page.goto(url)
+        await page.goto(URL_FOR_DRESSES)
         print('page title ', await page.title())
         # find number of dresses in catagory
-        await page.locator(':nth-match(:text("abc"), 27)').click();
-
-        
-        
+        await asyncio.sleep(10)
+        # await expect(page.get_by_text("GO!")).to_be_visible()
+        await page.get_by_role("button", name=re.compile("GO!", re.IGNORECASE)).click()
+        # await page.locator(':nth-match(:text("abc"), 27)').click()
+        await asyncio.sleep(10)
+        await page.goto(URL_FOR_DRESSES)
+        await asyncio.sleep(10)
         await browser.close()
-
-
-
-
 
 
 if __name__ == '__main__':
